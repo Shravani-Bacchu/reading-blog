e0f6c5952260b070f31244be1250103c96ed5fea
Shravani-Bacchu <shravanibacchu@gmail.com>
2025-12-31 20:22:07 +0000

    edited about page

--- src/components/BookSearch.jsx
+++
@@ -1,8 +0,0 @@
-export default async function searchBooks() {
-  const genre = document.querySelector("#genre").value;
-
-  const response = await fetch(`/api/books?genre=${genre}`);
-  const data = await response.json();
-
-  console.log(data);
-}
